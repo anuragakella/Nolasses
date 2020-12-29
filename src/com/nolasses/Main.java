2ade1394cafa6edd4ad16b44a8342c62058b8f9d
Anurag Akella <anuragakellaak@gmail.com>
2020-12-29 14:38:41 +0530

    Update Main.java

--- src/com/nolasses/Main.java
+++ src/com/nolasses/Main.java
@@ -5,14 +5,6 @@ import java.awt.event.ActionListener;
 
 public class Main {
     public static void main(String[] args) {
-//        //char timeFormat[] = {'*', '*', ':', '*', '*', ':', '*', '*'};
-//        LinkStore store = new LinkStore();
-//        store.addLink("20:10:00", "https://apple.com/");
-//        LinkTracker linkTracker = new LinkTracker(store.getLinks());
-//        Thread linkTrackerThread = new Thread(linkTracker);
-//        linkTrackerThread.start();
-//        store.addLink("20:10:43", "https://apple.com/");
-//        linkTracker.updateLinks(store.getLinks());
         NolassesBridge nolassesBridge = new NolassesBridge();
     }
 }
